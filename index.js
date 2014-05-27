8d0c904c79fe80e99706f51f55727c0e4d71d218
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-05-27 10:51:19 -0400

    working on jade campaign

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 'use strict';
 
-var mustache = require('mustache');
+var jade = require('jade');
 var path = require('path');
 var fs = require('fs');
 var cache = {};
 
 function read (file, done) {
-    if (file in cache) {
-        return process.nextTick(next);
-    }
+  if (file in cache) {
+    next(); return;
+  }
 
-    fs.readFile(file, { encoding: 'utf8' }, function (err, template) {
-        if (err) {
-            done(err);
-        } else {
-            mustache.parse(template);
-            cache[file] = template;
-            next();
-        }
-    });
-
-    function next () {
-        done(null, cache[file]);
+  fs.readFile(file, { encoding: 'utf8' }, function (err, template) {
+    if (err) {
+      done(err);
+    } else {
+      jade.parse(template);
+      cache[file] = template;
+      next();
     }
+  });
+
+  function next () {
+    done(null, cache[file]);
+  }
 }
 
 module.exports = {
-    defaultLayout: path.join(__dirname, 'layouts/layout.mu'),
-    render: function (file, model, done) {
-        read(file, function (err, template) {
-            if (err) {
-                done(err);
-            } else {
-                done(null, mustache.render(template, model));
-            }
-        });
-    },
-    renderString: function (template, model, done) {
-        done(null, mustache.render(template, model));
-    }
+  defaultLayout: path.join(__dirname, 'layout.jade'),
+  render: function (file, model, done) {
+    read(file, function (err, template) {
+      if (err) {
+        done(err);
+      } else {
+        done(null, jade.render(template, model));
+      }
+    });
+  },
+  renderString: function (template, model, done) {
+    done(null, jade.render(template, model));
+  }
 };
